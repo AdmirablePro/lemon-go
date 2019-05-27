39b71b118d9843cdc34efc6c75afea5eba256f12
Frederic Chan <frederic.t.chan@gmail.com>
2019-05-27 18:45:32 +0800

    fix for adapting to channel

--- lemon/fetch.go
+++ lemon/fetch.go
@@ -13,41 +13,41 @@ import (
 func fetchTask(taskChannel chan<- Task) {
 	for {
 		time.Sleep(time.Second) // sleep 1s
-		if taskList.Len() < *maxQueueSize {
-			// get task
-			resp, err := http.Get(*serverAddress + "/task?num=" + strconv.Itoa(*maxQueueSize-taskList.Len()))
-			if err != nil {
-				MetricAddOne(FetchFailed)
-				raven.CaptureErrorAndWait(err, nil)
-				logger.Warnf(currentLangBundle.FetchingTaskError, err.Error())
-				continue
-			}
 
-			if resp.StatusCode != 200 {
-				MetricAddOne(FetchFailed)
-				raven.CaptureErrorAndWait(err, nil)
-				respBody, _ := ioutil.ReadAll(resp.Body)
-				logger.Warnf(currentLangBundle.FetchingTaskNon200, resp.StatusCode, string(respBody))
-				continue
-			}
-
-			// decode list
-			var tasks []Task
-			err = json.NewDecoder(resp.Body).Decode(&tasks)
-			if err != nil {
-				MetricAddOne(FetchFailed)
-				raven.CaptureErrorAndWait(err, nil)
-				logger.Warnf(currentLangBundle.FetchingTaskDecodeError, err.Error())
-				continue
-			}
-
-			logger.Debugf(currentLangBundle.FetchTaskCount, len(tasks))
-
-			// save to queue
-			for _, item := range tasks {
-				taskChannel <- item
-				MetricAddOne(TaskReceived)
-			}
+		// get task
+		resp, err := http.Get(*serverAddress + "/task?num=" + strconv.Itoa(*maxQueueSize-len(taskChannel)))
+		if err != nil {
+			MetricAddOne(FetchFailed)
+			raven.CaptureErrorAndWait(err, nil)
+			logger.Warnf(currentLangBundle.FetchingTaskError, err.Error())
+			continue
 		}
+
+		if resp.StatusCode != 200 {
+			MetricAddOne(FetchFailed)
+			raven.CaptureErrorAndWait(err, nil)
+			respBody, _ := ioutil.ReadAll(resp.Body)
+			logger.Warnf(currentLangBundle.FetchingTaskNon200, resp.StatusCode, string(respBody))
+			continue
+		}
+
+		// decode list
+		var tasks []Task
+		err = json.NewDecoder(resp.Body).Decode(&tasks)
+		if err != nil {
+			MetricAddOne(FetchFailed)
+			raven.CaptureErrorAndWait(err, nil)
+			logger.Warnf(currentLangBundle.FetchingTaskDecodeError, err.Error())
+			continue
+		}
+
+		logger.Debugf(currentLangBundle.FetchTaskCount, len(tasks))
+
+		// save to queue
+		for _, item := range tasks {
+			taskChannel <- item
+			MetricAddOne(TaskReceived)
+		}
+
 	}
 }
